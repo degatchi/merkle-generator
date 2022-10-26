1ffadc18c0cc0445c7f3fc2ed9b5644a89136aa4
DeGatchi <DeGatchi@users.noreply.github.com>
2022-10-26 17:21:17 +1100

    add: quickstart readme

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use merkle_generator::MerkleTree;
+
+fn main() {
+    println!("Generating the Merkle Tree.");
+    MerkleTree::import();
+    println!("DONE: The output is found at src/output.json");
+}
