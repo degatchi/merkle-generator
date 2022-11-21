f5bdeab4e4db7e1cefb76cb817d24eb27bfc3143
0xClandestine <clandestine.eth@proton.me>
2022-11-21 15:19:56 -0600

    ✨clap cli support

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use clap::Parser;
 use merkle_generator::MerkleTree;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, required = true)]
+    input_path: String,
+
+    #[arg(short, long, required = true)]
+    output_path: String,
+}
+
 fn main() {
-    println!("Generating the Merkle Tree.");
-    MerkleTree::import();
-    println!("DONE: The output is found at src/output.json");
+    let args = Args::parse();
+
+    println!("Generating the Merkle Tree...");
+
+    MerkleTree::import(&args.input_path, &args.output_path);
+
+    println!("DONE: The output is found at {}", args.output_path);
 }
